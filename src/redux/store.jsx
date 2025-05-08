5cf9fb708992c167fb2aaa312da154e3df05a1dd
Altaf Parves <altafparvesh@gmail.com>
2025-05-09 02:59:44 +0700

    feat: Get Story Data from api

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import xpReducer from "./features/xp/XpSlice";
+import storiesReducer from "./features/Story/StorySlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     xp: xpReducer,
+    stories: storiesReducer,
   },
 });
