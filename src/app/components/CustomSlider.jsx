4d0b7098e0fa24962c8e7827c886bb9931704608
Altaf Parves <altafparvesh@gmail.com>
2025-05-10 03:59:48 +0700

    fix: customSlider style

--- src/app/components/CustomSlider.jsx
+++ src/app/components/CustomSlider.jsx
@@ -1,30 +1,32 @@
-'use client';
+"use client";
 import { useState } from "react";
 
-export default function CustomSlider() {
-  const [value, setValue] = useState(50);
+export default function CustomSlider({ onChange }) {
+  const [value, setValue] = useState(5); 
+
+  const handleChange = (e) => {
+    const newValue = Number(e.target.value);
+    setValue(newValue);
+    onChange?.(newValue);
+  };
+
+  console.log("this is value of slider", value);
 
   return (
     <div className="flex flex-col items-center w-full px-6">
-      {/* Slider container */}
       <div className="relative w-full max-w-md">
-        {/* Background Track */}
-        <div className="h-10 rounded-full bg-gray-300 relative overflow-hidden">
-          {/* Active Fill */}
-          <div className="h-full bg-blue-500 absolute top-0 left-0 rounded-full" style={{ width: `${value}%` }}></div>
-          {/* Thumb */}
+        <div className="h-10 rounded-full bg-[#C7C7C7] relative overflow-hidden">
+          <div className="h-full bg-blue-500 absolute top-0 left-0 rounded-tl-full rounded-bl-full" style={{ width: `${(value - 1) * 11.11}%` }}></div>
           <div
-            className="absolute top-1/2 transform -translate-y-1/2 bg-white border-4 border-blue-500 rounded-full"
+            className="absolute top-1/2 transform -translate-y-1/2 bg-[#F6F5F4]  rounded-full"
             style={{
-              left: `calc(${value}% - 20px)`,
+              left: `calc(${(value - 1) * (100 / 9)}% - ${value === 1 ? 0 : value === 10 ? 40 : 20}px)`,
               width: "40px",
               height: "40px",
             }}
           ></div>
         </div>
-
-        {/* Invisible Input Range */}
-        <input type="range" min="0" max="100" value={value} onChange={(e) => setValue(e.target.value)} className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
+        <input type="range" min="1" max="10" value={value} onChange={handleChange} className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
       </div>
     </div>
   );
