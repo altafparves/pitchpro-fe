2988ae6ac427b37611143a8ceed676b95b74b2f5
Altaf Parves <altafparvesh@gmail.com>
2025-05-10 03:42:58 +0700

    feat: add onclick props in button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -8,11 +8,11 @@ const VARIANT_CLASSES = {
   danger: "bg-red-500 text-white shadow-[0px_2px_0px_0px_#B91C1C]",
 };
 
-const Button = ({ children, width = "w-full", className = "", type = "button", variant = "primary",disabled = false, ...props }) => {
+const Button = ({ children,onClick, width = "w-full", className = "", type = "button", variant = "primary",disabled = false, ...props }) => {
   const variantClass = VARIANT_CLASSES[variant] || VARIANT_CLASSES.primary;
   const disabledClass = disabled ? "bg-neutral-200 text-neutral-300 shadow-[0px_2px_0px_0px_#A6A6A6] cursor-not-allowed" : "";
   return (
-    <button type={type} className={`py-3 flex rounded-full items-center justify-center text-body font-[550] ${width} ${variantClass} ${className}  ${disabledClass}`} disabled={disabled} {...props}>
+    <button onClick={onClick} type={type} className={`py-3 flex rounded-full items-center justify-center text-body font-[550] ${width} ${variantClass} ${className}  ${disabledClass}`} disabled={disabled} {...props}>
       {children}
     </button>
   );
@@ -23,6 +23,7 @@ Button.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
   disabled: PropTypes.bool,
+  onClick: PropTypes.func,
   variant: PropTypes.oneOf(["primary", "info", "success", "danger"]),
 };
 
