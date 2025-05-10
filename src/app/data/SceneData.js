8a627c725ac644d60e98373babcb5bf7b06a7635
Altaf Parves <altafparvesh@gmail.com>
2025-05-10 23:36:13 +0700

    refactor: Scene data based on position

--- src/app/data/SceneData.js
+++ src/app/data/SceneData.js
@@ -8,14 +8,6 @@ export const sceneData = [
     status: "unlocked",
     x: 0,
     y: 1,
-    src: {
-      checkpoint: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2)StudyWith(CLICK).mp4",
-      },
-      cutscene: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(1)Pilih%20belajar(CS).mp4",
-      },
-    },
   },
   {
     id: 2,
@@ -26,19 +18,6 @@ export const sceneData = [
     status: "unlocked",
     x: 2,
     y: 0,
-    src: {
-      checkpoint: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2.A.1)Persuasive(AUDIO).mp4",
-      },
-      cutscene: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2.A)Teman(CS).mp4",
-        2: "https://storage.cloud.google.com/assets-pitchpro/(2.A.1.A)Agree(CS).mp4",
-        3: "https://storage.cloud.google.com/assets-pitchpro/(2.A.2)DimasCame(CS).mp4",
-        4: "https://storage.cloud.google.com/assets-pitchpro/(2.A.3)ReadSlide(CS).mp4",
-        // different branch
-        5: "https://storage.cloud.google.com/assets-pitchpro/(2.A.4)Confused(CS).mp4",
-      },
-    },
   },
   {
     id: 3,
@@ -47,14 +26,6 @@ export const sceneData = [
     status: "locked",
     x: 2,
     y: 1,
-    src: {
-      cutscene: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2.B)Sendiri(CS).mp4",
-        2: "https://storage.cloud.google.com/assets-pitchpro/(2.A.6.1)OpeningExplanation(CS).mp4",
-        3: "https://storage.cloud.google.com/assets-pitchpro/(2.A.6.2)MainExplanation(CS).mp4",
-        4: "https://storage.cloud.google.com/assets-pitchpro/(2.A.6.3)ClosingExplanation(CS).mp4",
-      },
-    },
   },
   {
     id: 4,
@@ -69,19 +40,27 @@ export const sceneData = [
     label: "cutscene 3",
     type: "cutscene",
     status: "locked",
-    x: 3,
-    y: 0,
+    x: 6,
+    y: 2,
   },
   {
     id: 6,
     label: "cutscene 4",
     type: "cutscene",
     status: "locked",
-    x: 4,
+    x: 3,
     y: 0,
   },
   {
     id: 7,
+    label: "cutscene 5",
+    type: "cutscene",
+    status: "locked",
+    x: 4,
+    y: 0,
+  },
+  {
+    id: 8,
     label: "material 1",
     type: "material",
     status: "locked",
@@ -89,14 +68,9 @@ export const sceneData = [
     desc: "Breathing execise for optimize your presentation and releave your anxiety",
     x: 5,
     y: 0,
-    src: {
-      material: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2.A.5)BreatheExcercise(CS).mp4",
-      },
-    },
   },
   {
-    id: 8,
+    id: 9,
     label: "checkpoint 2",
     type: "checkpoint",
     title: "Presentation with friend",
@@ -104,72 +78,32 @@ export const sceneData = [
     status: "locked",
     x: 6,
     y: 0,
-    src: {
-      checkpoint: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7)1stAnswer(AUDIO)mp4.mp4",
-        2: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7)2ndAnswer(AUDIO)mp4.mp4",
-        3: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7)3rdAnswer(AUDIO)mp4.mp4",
-        4: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7)4thAnswer(AUDIO)mp4.mp4",
-        5: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7)1stAnswer(AUDIO)mp4.mp4",
-      },
-      cutscene: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7.1)30S_Opening(CS).mp4",
-        2: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7.2)1M_Differences(CS).mp4",
-        3: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7.3)1M_Ethics(CS).mp4",
-        4: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7.4)1M_Consequences(CS).mp4",
-        5: "https://storage.cloud.google.com/assets-pitchpro/(2.A.7.5)30S_Closing(CS).mp4",
-        // different part cut scene
-        6: "https://storage.cloud.google.com/assets-pitchpro/(2.A.8)FinishExcercise(CS).mp4",
-      },
-    },
   },
   {
-    id: 9,
+    id: 10,
     label: "cutscene 6",
     type: "cutscene",
     status: "locked",
     x: 6,
     y: 1,
   },
-  {
-    id: 10,
-    label: "cutscene 7",
-    type: "cutscene",
-    status: "locked",
-    x: 6,
-    y: 2,
-  },
+
   {
     id: 11,
     label: "checkpoint 3",
     type: "checkpoint",
     title: "Presentation with lecturer",
-    desc:" Arga presents in front of his lecture at his class. Prepare your answer for QnA session.",
+    desc: " Arga presents in front of his lecture at his class. Prepare your answer for QnA session.",
     status: "locked",
     x: 7,
     y: 1,
-    src: {
-      checkpoint: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(3.1)1stAnswer(AUDIO).mp4",
-        2: "https://storage.cloud.google.com/assets-pitchpro/(3.3)2ndAnswer(AUDIO).mp4",
-      },
-      cutscene: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(3)1stQnA(CS).mp4",
-        2: "https://storage.cloud.google.com/assets-pitchpro/(3.2)2ndQnA(CS).mp4",
-      },
-    },
   },
   {
     id: 12,
-    label: "cutscene 8",
+    label: "cutscene 7",
     type: "cutscene",
     status: "locked",
     x: 8,
     y: 1,
-    src: {
-      cutscene: {
-        1: "https://storage.cloud.google.com/assets-pitchpro/(3.4)PresentationFinish(CS).mp4",
-      },
-    },
   },
 ];
