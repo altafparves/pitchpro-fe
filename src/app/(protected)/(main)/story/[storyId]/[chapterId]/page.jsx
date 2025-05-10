5880200158e82b7fa70819f989b1421835419f2e
Altaf Parves <altafparvesh@gmail.com>
2025-05-10 18:09:20 +0700

    feat: Add page transition to chapter detail

--- src/app/(protected)/(main)/story/[storyId]/[chapterId]/page.jsx
+++ src/app/(protected)/(main)/story/[storyId]/[chapterId]/page.jsx
@@ -6,29 +6,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import SceneGraph from "@/app/components/SceneGraph";
 import PopupInfo from "@/app/components/PopupInfo";
+import PageTransitionWrapper from "@/app/animation/PageTransition";
 import { SelectedNodeProvider } from "@/app/context/SelectedNodeContext";
 export default function ChapterDetail() {
   const router = useRouter();
   const params = useParams();
   const storyId = params?.storyId;
   return (
-    <SelectedNodeProvider>
-    <div className="bg-neutral-50 overflow-y-hidden w-full min-h-screen flex flex-col relative ">
-      <div className="bg-neutral-50 h-[104px] flex items-end justify-between py-2 fixed w-full z-40 px-6">
-        <button onClick={() => router.back()} className="flex flex-row gap-2 items-center p-2 pl-0 text-body text-neutral-300 font-semibold text">
-          <FontAwesomeIcon icon={faArrowLeft} className="w-[29px] h-[29px]" />
-          Choose Story
-        </button>
-        <XpChip></XpChip>
-      </div>
-      <PopupInfo title="Chapter 1" desc="Ted will prepare his final presentation"></PopupInfo>
-      {/* story journey section */}
-      <div className="w-full mt-[104px] flex-grow relative overflow-x-auto">
-        <div className="flex absolute top-0 bottom-0 items-center flex-grow overflow-y-hidden w-max px-8 gap-10 ">
-          <SceneGraph />
+    <PageTransitionWrapper>
+      <SelectedNodeProvider>
+        <div className="bg-neutral-50 overflow-y-hidden w-full min-h-screen flex flex-col relative ">
+          <div className="bg-neutral-50 h-[94px] flex items-end justify-between py-2 fixed w-full z-40 px-6">
+            <button onClick={() => router.back()} className="flex flex-row gap-2 items-center p-2 pl-0 text-body text-neutral-300 font-semibold text">
+              <FontAwesomeIcon icon={faArrowLeft} className="w-[29px] h-[29px]" />
+              Choose Story
+            </button>
+            <XpChip></XpChip>
+          </div>
+          <PopupInfo title="Chapter 1" desc="Arga, who is collage student, want to  prepare his AI class presentation. "></PopupInfo>
+          {/* story journey section */}
+          <div className="w-full mt-[206px]  flex-grow relative overflow-x-auto">
+            <div className="flex absolute top-0 bottom-0 items-center flex-grow overflow-y-hidden w-max px-8 gap-10 ">
+              <SceneGraph />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    </SelectedNodeProvider>
+      </SelectedNodeProvider>
+    </PageTransitionWrapper>
   );
 }
