bb11b6c869e3c56cad4f7b0dbc1d11f3bf077615
Altaf Parves <altafparvesh@gmail.com>
2025-05-10 22:42:55 +0700

    fix: Fix story detail page

--- src/app/(protected)/(main)/story/[storyId]/page.jsx
+++ src/app/(protected)/(main)/story/[storyId]/page.jsx
@@ -1,32 +1,28 @@
 "use client";
-import { useRouter } from "next/navigation";
 import XpChip from "@/app/components/XpChip";
-import StoryCard from "@/app/components/StoryCard";
 import PageTransitionWrapper from "@/app/animation/PageTransition";
-import ProtectedRoute from "@/app/(protected)/layout";
 import ChapterCard from "@/app/components/ChapterCard";
 export default function StoryDetail() {
-  const router = useRouter();
   return (
     <>
-        <PageTransitionWrapper transitionType="dissolve">
-          <div className="bg-neutral-50 overflow-y-auto w-full min-h-screen flex flex-col relative ">
-            <div className="bg-neutral-50 h-[104px] flex items-end justify-end py-2 fixed w-full z-40 px-6">
-              <XpChip></XpChip>
-            </div>
+      <PageTransitionWrapper transitionType="dissolve">
+        <div className="bg-neutral-50 overflow-y-auto w-full min-h-screen flex flex-col relative ">
+          <div className="bg-neutral-50 h-[104px] flex items-end justify-end py-2 fixed w-full z-40 px-6">
+            <XpChip></XpChip>
+          </div>
 
-            <div className="flex-1 bg-neutral-50 mt-[104px] overflow-y-auto px-6 pb-[104px] flex flex-col gap-8 ">
-              <div className="w-full flex flex-col items-start gap-4 text-neutral-900">
-                <p className="text-heading-h1 font-semibold">Chapter</p>
-              </div>
-              {/* story container */}
-              <div className="w-full flex flex-col gap-8">
-                {/* story */}
-                <ChapterCard chapterId={1} title="Chapter 1" description="Practice with friend and final presentation with lecture"></ChapterCard>
-              </div>
+          <div className="flex-1 bg-neutral-50 mt-[104px] overflow-y-auto px-6 pb-[104px] flex flex-col gap-8 ">
+            <div className="w-full flex flex-col items-start gap-4 text-neutral-900">
+              <p className="text-heading-h1 font-semibold">Chapter</p>
+            </div>
+            {/* story container */}
+            <div className="w-full flex flex-col gap-8">
+              {/* story */}
+              <ChapterCard chapterId={1} title="Chapter 1" description="Practice with friend and final presentation with lecture"></ChapterCard>
             </div>
           </div>
-        </PageTransitionWrapper>
+        </div>
+      </PageTransitionWrapper>
     </>
   );
 }
