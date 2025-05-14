ebcb45622e956e56f5eea4ce161b1136edc531f9
Altaf Parves <altafparvesh@gmail.com>
2025-05-14 13:39:13 +0700

    feat: Add Posttest feature

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -6,12 +6,14 @@ import pretestReducer from "./features/Pretest/preTestSlice";
 import audioReducer from "./features/Audio/audioSlice";
 import feedbackReducer from "./features/feedback/feedbackSlice";
 import profileReducer from "./features/profile/profileSlice";
+import posttestReducer from "./features/PostTest/postTestSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     xp: xpReducer,
     stories: storiesReducer,
     pretest: pretestReducer,
+    posttest: posttestReducer,
     audio: audioReducer,
     feedback: feedbackReducer,
     profile: profileReducer,
