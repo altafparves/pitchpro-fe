2f01b3eb7145c690330789c1a4f71e8cca7df091
Altaf Parves <altafparvesh@gmail.com>
2025-05-15 15:02:30 +0700

    Feat: Add PWA Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// import nextPWA from "next-pwa";
+
+// const nextConfig = {
+//   };
+  
+//   const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development',
+//   });
+  
+//   module.exports = withPWA(nextConfig);
+
+// next.config.mjs
+import nextPWA from "next-pwa";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your other Next.js config if any
+};
+
+const withPWA = nextPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default withPWA(nextConfig);
